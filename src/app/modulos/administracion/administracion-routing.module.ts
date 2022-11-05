41bdc4920e5f76e4c47a17b93ef91fdbee723f9e
Alejandro Marin <alejma7@gmail.com>
2022-11-04 20:50:35 -0500

    Se implementa mensaje de error para rutas no existentes y se crea la parte visual del login

--- src/app/modulos/administracion/administracion-routing.module.ts
+++ src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearEmpleadoComponent } from './empleados/crear-empleado/crear-empleado.component';
+import { EditarEmpleadoComponent } from './empleados/editar-empleado/editar-empleado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'crear-empleado',
+    component: CrearEmpleadoComponent
+  },
+  {
+    path: 'editar-empleado',
+    component: EditarEmpleadoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
